d787d54e9013100531bdab9e9bbe94c0bc26b8b4
harbor <276446675@qq.com>
2018-06-06 12:01:21 +0800

    first_commit

---
+++ src/main/java/com/harbor/chart/ChartApplication.java
@@ -0,0 +1,12 @@
+package com.harbor.chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartApplication.class, args);
+	}
+}
