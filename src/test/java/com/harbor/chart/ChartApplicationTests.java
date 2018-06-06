23ea41788e87987a357d9c7f4a30ccf69a21a7ca
harbor <276446675@qq.com>
2018-06-06 12:25:19 +0800

    修改jdk_version to1.7 && 修改spring boot_version to 1.3.6  && 增加相关jar依赖

--- src/test/java/com/harbor/chart/ChartApplicationTests.java
+++ src/test/java/com/harbor/chart/ChartApplicationTests.java
@@ -2,11 +2,8 @@ package com.harbor.chart;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class ChartApplicationTests {
 
 	@Test
